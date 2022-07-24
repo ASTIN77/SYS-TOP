863fc79b7fdede5cbe2711185237fbb138ccb53d
Andrew Tinsley <andrew@astinsley.net>
2022-07-24 17:36:35 +0100

    Changed status to production for packaging

--- AppTray.js
+++ AppTray.js
@@ -3,31 +3,29 @@ const { app, Menu, Tray } = require("electron");
 class AppTray extends Tray {
   constructor(icon, mainWindow) {
     super(icon);
-
     this.mainWindow = mainWindow;
-    this.on("click", this.onClick.bind(this));
-    this.on("right-click", this.onRightClick.bind(this));
-  }
+    this.setToolTip("SysTop");
 
-  onClick() {
-    if (this.mainWindow.isVisible() === true) {
-      this.mainWindow.hide();
-    } else {
-      this.mainWindow.show();
-    }
-  }
+    this.on("click", () => {
+      if (this.mainWindow.isVisible() === true) {
+        this.mainWindow.hide();
+      } else {
+        this.mainWindow.show();
+      }
+    });
 
-  onRightClick() {
-    const contextMenu = Menu.buildFromTemplate([
-      {
-        label: "Quit",
-        click: () => {
-          app.isQuitting = true;
-          app.quit();
+    this.on("right-click", () => {
+      const contextMenu = Menu.buildFromTemplate([
+        {
+          label: "Quit",
+          click: () => {
+            app.isQuitting = true;
+            app.quit();
+          },
         },
-      },
-    ]);
-    this.popUpContextMenu(contextMenu);
+      ]);
+      this.popUpContextMenu(contextMenu);
+    });
   }
 }
 
